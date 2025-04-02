d226e55c93e44d29d89522eb4c6f31379b49319a
qeautomationkatalon@gmail.com <katalon_g3_dummy_username>
2025-04-02 07:19:45 +0000

    Delete "Delete User Account Confirmation" test case

--- Test Suites/Test Suite Katalon Automated TC.ts
+++ Test Suites/Test Suite Katalon Automated TC.ts
@@ -1,61 +1,51 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-   <description></description>
-   <name>Test Suite Katalon Automated TC</name>
-   <tag></tag>
-   <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>6bf1c7af-57f5-48a3-b490-bd1be49e80ae</testSuiteGuid>
-   <testCaseLink>
-      <guid>e9161126-eac9-423f-8a65-079d579819a6</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/Add Item to Shopping Cart</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>717133b8-8839-4ca1-a55f-72706412a9c2</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/API Response Time Validation</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>602d6c8c-d6dd-40d2-9d24-4a1085fb094a</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/Checkout with Credit Card</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>d7667df3-e376-428b-8de0-78eaea69812e</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>a84ceb2a-b355-4a0a-b188-833e543e9f60</variableId>
-      </variableLink>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d1210e5e-0921-4f4d-b74d-e9b1c2b2b8d7</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/Delete User Account Confirmation</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>cb404f77-63a8-4444-9f3a-d5f84d41ac38</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/Login with Invalid Password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
+    <name>Test Suite Katalon Automated TC</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>3</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>true</rerunImmediately>
+    <testSuiteGuid>6bf1c7af-57f5-48a3-b490-bd1be49e80ae</testSuiteGuid>
+    <testCaseLink>
+        <guid>e9161126-eac9-423f-8a65-079d579819a6</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Katalon Automated TC/Add Item to Shopping Cart</testCaseId>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>717133b8-8839-4ca1-a55f-72706412a9c2</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Katalon Automated TC/API Response Time Validation</testCaseId>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>602d6c8c-d6dd-40d2-9d24-4a1085fb094a</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Katalon Automated TC/Checkout with Credit Card</testCaseId>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>d7667df3-e376-428b-8de0-78eaea69812e</variableId>
+        </variableLink>
+        <variableLink>
+            <testDataLinkId></testDataLinkId>
+            <type>DEFAULT</type>
+            <value></value>
+            <variableId>a84ceb2a-b355-4a0a-b188-833e543e9f60</variableId>
+        </variableLink>
+    </testCaseLink>
+    <testCaseLink>
+        <guid>cb404f77-63a8-4444-9f3a-d5f84d41ac38</guid>
+        <isReuseDriver>false</isReuseDriver>
+        <isRun>true</isRun>
+        <testCaseId>Test Cases/Katalon Automated TC/Login with Invalid Password</testCaseId>
+    </testCaseLink>
 </TestSuiteEntity>
+
