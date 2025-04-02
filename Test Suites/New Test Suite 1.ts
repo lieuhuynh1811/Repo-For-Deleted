adf2a045a831dd61db0ce73dff08ca7cb05f6c12
qeautomationkatalon@gmail.com <lieuhuynh1811>
2025-04-02 04:30:40 +0000

    Delete 2 test case(s)

--- Test Suites/New Test Suite 1.ts
+++ Test Suites/New Test Suite 1.ts
@@ -1,40 +1,15 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
-   <description></description>
-   <name>New Test Suite 1</name>
-   <tag></tag>
-   <isRerun>false</isRerun>
-   <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
-   <pageLoadTimeout>30</pageLoadTimeout>
-   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
-   <testSuiteGuid>9c28e3b0-e454-4678-a4fd-f98bbabf2ebd</testSuiteGuid>
-   <testCaseLink>
-      <guid>20e188ca-d0c4-485a-b2b0-894125bd877c</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/Login with Invalid Password</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-   </testCaseLink>
-   <testCaseLink>
-      <guid>d4344007-c373-4a8e-81e8-9c3171783233</guid>
-      <isReuseDriver>false</isReuseDriver>
-      <isRun>true</isRun>
-      <testCaseId>Test Cases/Katalon Automated TC/Login with Valid Credentials</testCaseId>
-      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>14d98e7b-4ac8-4e28-8ce7-394001d188f0</variableId>
-      </variableLink>
-      <variableLink>
-         <testDataLinkId></testDataLinkId>
-         <type>DEFAULT</type>
-         <value></value>
-         <variableId>a97344e7-5f9c-4936-92a2-d85e5463f3c9</variableId>
-      </variableLink>
-   </testCaseLink>
+    <name>New Test Suite 1</name>
+    <description></description>
+    <tag></tag>
+    <isRerun>false</isRerun>
+    <mailRecipient></mailRecipient>
+    <numberOfRerun>0</numberOfRerun>
+    <pageLoadTimeout>30</pageLoadTimeout>
+    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+    <rerunImmediately>false</rerunImmediately>
+    <testSuiteGuid>9c28e3b0-e454-4678-a4fd-f98bbabf2ebd</testSuiteGuid>
 </TestSuiteEntity>
+
