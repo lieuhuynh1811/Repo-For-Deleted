46c7820389551fad96b3d4ed511bdc50243351ae
lieuhuynh1811 <lieu.huynh@katalon.com>
2025-04-02 11:45:33 +0700

    update

--- Test Suites/New Test Suite 1.ts
+++ Test Suites/New Test Suite 1.ts
@@ -1,15 +1,33 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-    <name>New Test Suite 1</name>
-    <description></description>
-    <tag></tag>
-    <isRerun>false</isRerun>
-    <mailRecipient></mailRecipient>
-    <numberOfRerun>0</numberOfRerun>
-    <pageLoadTimeout>30</pageLoadTimeout>
-    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
-    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-    <rerunImmediately>false</rerunImmediately>
-    <testSuiteGuid>9c28e3b0-e454-4678-a4fd-f98bbabf2ebd</testSuiteGuid>
+   <description></description>
+   <name>New Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>9c28e3b0-e454-4678-a4fd-f98bbabf2ebd</testSuiteGuid>
+   <testCaseLink>
+      <guid>d00128a5-baed-4e12-ba55-7593edade2b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Katalon Automated TC/Checkout with Credit Card</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>d7667df3-e376-428b-8de0-78eaea69812e</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a84ceb2a-b355-4a0a-b188-833e543e9f60</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
-
